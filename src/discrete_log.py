3b284e7ed0da098c92301d06e4d7d732dfeb9806
inkompotato <fra.js.1998@gmail.com>
2019-11-28 09:52:38 +0100

    added more discrete log stuff

--- src/discrete_log.py
+++ src/discrete_log.py
@@ -9,29 +9,47 @@ def multiplications(x, y, n):
     print(y_bin)
     length = len(y_bin)
     array_powers = []
-    result = 1
 
     for i in range(length):
         if y_bin[i] == '1':
             array_powers.append(length-1-i)
+
     print(array_powers)
     array_powers.reverse()
 
-    for num in array_powers:
-        result *= pow(x, pow(2, num))
+    temp = 1
 
+    for num in array_powers:
+        temp *= pow(x, pow(2, num), n)
+
+    result = temp % n
     end = current_time_ms()
-    print(result%n)
+
+    print(result)
     print(end - start)
 
 
-multiplications(2, 1234567890, 11)
+# multiplications(5432132876543212345678, 34156812365437654387654328765436543, 12654654)
 
 
 def multiplications_easy(x, y, n):
     start = current_time_ms()
-    print(pow(x, y, n))
+    print(pow(x, y) % n)
     print(current_time_ms()-start)
 
 
-multiplications_easy(2, 1234567890, 11)
+# multiplications_easy(5432132876543212345678, 34156812365437654387654328765436543, 12654654)
+
+
+def discrete_log(p, a, b):
+    if p < a or p < b: return
+    for x in range(pow(2, p)):
+        if pow(a, x, p) == b:
+            print(x)
+            return
+    print(0)
+
+
+discrete_log(281, 17, 91)
+
+# print(pow(3, 11, 11))
